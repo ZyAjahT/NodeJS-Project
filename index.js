d95b6aecab59541c79212961aebd2ee29bf683fe
ZyAjahT <zyajah@gmail.com>
2025-03-28 17:55:35 -0400

    Added ejs package

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var app = express(); 
 const path = require('path'); 
 
-app.set('views', path.join(__dirname), 'views'); 
 app.set('view engine', 'ejs'); 
+app.set('views', path.join(__dirname), 'views'); 
 
-app.get('/', function(req,res){
-    res.render('index'); 
+app.get('/', function(req, res){
+    res.render("index");
 })
 
 app.listen(3000, function(){
