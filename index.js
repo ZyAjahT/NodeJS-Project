0e6f75d0a0d728403c689bd82d2af6f6b74c93b4
ZyAjahT <zyajah@gmail.com>
2025-03-27 23:43:14 -0400

    Added index.ejs and resume.ejs

---
+++ index.js
@@ -0,0 +1,14 @@
+var express = require('express'); 
+var app = express(); 
+const path = require('path'); 
+
+app.set('views', path.join(__dirname), 'views'); 
+app.set('view engine', 'ejs'); 
+
+app.get('/', function(req,res){
+    res.render('index'); 
+})
+
+app.listen(3000, function(){
+    console.log('Our app is running on port 3000'); 
+})
