78a653f69eb43a8eeeb8575e7fb03f7abb6ab9fe
ZyAjahT <zyajah@gmail.com>
2025-03-28 23:00:26 -0400

    Added render for resume.ejs

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', function(req, res){
     res.render('index');
 })
 
+app.get('/resume', function(req, res){
+   res.render('resume'); 
+})
+
 app.listen(3000, function(){
     console.log('Our app is running on port 3000'); 
 })
